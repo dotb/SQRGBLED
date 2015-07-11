879ce7c9884f03989217d29d0623757322d98e51
Bradley Clayton <bradley@squarepolka.com>
2015-07-11 13:55:49 +1000

    Adding SQRGBLED

---
+++ SQRGBLED.cpp
@@ -0,0 +1,69 @@
+//
+//  SQRGBLED.cpp
+//  
+//
+//  Created by Bradley Clayton on 7/30/12.
+//  Copyright (c) 2012 Squarepolka. All rights reserved.
+//
+
+
+#include "SQRGBLED.h"
+
+SQRGBLED::SQRGBLED()
+{
+    
+}
+
+void SQRGBLED::setup(int redPin, int greenPin, int bluePin)
+{
+    _redLED.setup(redPin);
+    _greenLED.setup(greenPin);
+    _blueLED.setup(bluePin);
+}
+
+void SQRGBLED::colour(int red, int green, int blue, long duration)
+{
+    _redLED.fadeToBrightness(red, duration);
+    _greenLED.fadeToBrightness(green, duration);
+    _blueLED.fadeToBrightness(blue, duration);
+    
+    // Immediately set the colour if the duration is 0
+    if (duration <= 0)
+    {
+        this->loop();
+    }
+}
+
+boolean SQRGBLED::isFading()
+{
+
+}
+
+boolean SQRGBLED::isPulsing()
+{
+
+}
+
+#pragma mark - SQSchedularDelegate Methods
+unsigned int SQRGBLED::executionInterval()
+{
+    return 0;
+}
+
+// Clock events
+void SQRGBLED::loop()
+{
+    // Forward loop event
+    if (_redLED.isFading)
+    {
+        _redLED.loop();
+    }
+    if (_greenLED.isFading)
+    {
+        _greenLED.loop();
+    }
+    if (_blueLED.isFading)
+    {
+        _blueLED.loop();
+    }
+}
---
+++ SQRGBLED.h
@@ -0,0 +1,35 @@
+//
+//  SQRGBLED.h
+//  
+//
+//  Created by Bradley Clayton on 7/30/12.
+//  Copyright (c) 2012 Squarepolka. All rights reserved.
+//
+
+#ifndef SQRGBLED_h
+#define SQRGBLED_h
+
+#include "Arduino.h"
+#include "SQSchedularProtocol.h"
+#include "SQLED.h"
+
+class SQRGBLED : public SQSchedularProtocol
+{
+public:
+    SQRGBLED();
+    virtual void setup(int redPin, int greenPin, int bluePin);
+    virtual void colour(int red, int green, int blue, long duration);
+	virtual boolean isFading();
+	virtual boolean isPulsing();
+
+    #pragma mark - SQSchedularDelegate Methods
+    virtual unsigned int executionInterval();
+    virtual void loop();
+
+private:
+    SQLED _redLED;
+    SQLED _greenLED;
+    SQLED _blueLED;
+};
+
+#endif
